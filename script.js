028f1c8dcb4a3eabbbbe89832bc627a528b00b11
eduardo_c_cruz <eduardocorreadacruz@gmail.com>
2025-03-05 17:03:35 -0300

    att_CSS,HTML,JS

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-// script.js  
-document.addEventListener('DOMContentLoaded', function() {  
-    const buttons = document.querySelectorAll('.show-more');  
+document.addEventListener('DOMContentLoaded', function() {
+    const showMoreButtons = document.querySelectorAll('.show-more');
+    const overlay = document.querySelector('.overlay');
 
-    buttons.forEach(button => {  
-        button.addEventListener('click', function() {  
-            const sectionId = this.dataset.section;  
-            const contentId = sectionId + '-content';  
-            const content = document.getElementById(contentId);  
+    showMoreButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const sectionId = this.dataset.section;
+            const content = document.getElementById(sectionId + '-content');
 
-            if (content.style.display === 'none' || content.style.display === '') {  
-                content.style.display = 'block';  
-                this.textContent = 'Mostrar Menos';  
-            } else {  
-                content.style.display = 'none';  
-                this.textContent = 'Mostrar Mais';  
-            }  
-        });  
-    });  
-});  
+            if (content) {
+                content.classList.add('active');
+                overlay.style.display = 'block';
+            }
+        });
+    });
+
+    overlay.addEventListener('click', function() {
+        const activeContent = document.querySelector('.hidden-content.active');
+        if (activeContent) {
+            activeContent.classList.remove('active');
+            this.style.display = 'none';
+        }
+    });
+});
+
+let profileButton = document.querySelector('.profile-button');
+let menuDropdown = document.querySelector('.menu-dropdown');
+
+profileButton.addEventListener('click', () => {
+    menuDropdown.classList.toggle('show');
+});
+
+window.addEventListener('click', (event) => {
+    if (!event.target.matches('.profile-button')) {
+        if (menuDropdown.classList.contains('show')) {
+            menuDropdown.classList.remove('show');
+        }
+    }
+});
+
