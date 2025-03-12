e32d4de7e08cfe5210cd886e593d4be0da533558
EDUARDO CORRÊA DA CRUZ <eduardo_c_cruz@estudante.sesisenai.org.br>
2025-03-12 11:47:26 -0300

    a

--- script.js
+++ script.js
@@ -42,3 +42,44 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const showMoreButtons = document.querySelectorAll('.show-more');
+    const overlay = document.querySelector('.overlay');
+
+    showMoreButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const sectionId = this.dataset.section;
+            const content = document.getElementById(sectionId + '-content');
+
+            if (content) {
+                content.classList.add('active');
+                overlay.style.display = 'block';
+            }
+        });
+    });
+
+    overlay.addEventListener('click', function() {
+        const activeContent = document.querySelector('.hidden-content.active');
+        if (activeContent) {
+            activeContent.classList.remove('active');
+            this.style.display = 'none';
+        }
+    });
+});
+
+let profileButton = document.querySelector('.profile-button');
+let menuDropdown = document.querySelector('.menu-dropdown');
+
+profileButton.addEventListener('click', () => {
+    menuDropdown.classList.toggle('show');
+});
+
+window.addEventListener('click', (event) => {
+    if (!event.target.matches('.profile-button')) {
+        if (menuDropdown.classList.contains('show')) {
+            menuDropdown.classList.remove('show');
+        }
+    }
+});
+
